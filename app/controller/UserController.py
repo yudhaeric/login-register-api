1e8cefc577702ee7b52baaebd4848b96d5f78c4c
yudhaeric <yudhaericpamungkas@gmail.com>
2022-06-11 16:59:52 +0700

    Creating POST method in UserController

--- app/controller/UserController.py
+++ app/controller/UserController.py
@@ -2,7 +2,11 @@ from app.model.user import User
 
 from app import response, app, db
 from flask import request
+from flask_jwt_extended import *
 
+import datetime
+
+# GET Method
 def index():
     try:
         user = User.query.all()
@@ -25,9 +29,54 @@ def singleObject(data):
         'id': data.id,
         'name': data.name,
         'email': data.email,
-        'password': data.password,
         'created_at': data.created_at,
         'updated_at': data.updated_at
     }
 
     return data
+
+# POST Method
+def save():
+    try:
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        users = User(name=name, email=email)
+        users.setPassword(password)
+        db.session.add(users)
+        db.session.commit()
+
+        return response.success('', 'Registrasi Berhasil!')
+    except Exception as e:
+        print(e)
+
+# def login():
+#     try:
+#         email = request.form.get('email')
+#         password = request.form.get('password')
+
+#         user = User.query.filter_by(email=email).first()
+
+#         if not user:
+#             return response.badRequest([], 'Email kamu belum terdaftar nih!')
+
+#         if not user.checkPassword(password):
+#             return response.badRequest([], 'Password kamu salah!')
+
+#         data = singleObject(user)
+
+#         expires = datetime.timedelta(days=7)
+#         expires_refresh = datetime.timedelta(days=7)
+
+#         access_token = create_access_token(data, fresh=True, expires_delta= expires)
+#         refresh_token = create_refresh_token(data, expires_delta=expires_refresh)
+
+#         return response.success({
+#             "data" : data,
+#             "access_token" : access_token,
+#             "refresh_token" : refresh_token,
+#         }, "Login berhasil, selamat datang!")
+
+#     except Exception as e:
+#         print(e)
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
+from flask import request
 from app import app
 from app.controller import UserController
+from app.model.user import User
 
 @app.route('/')
 def index():
     return "Hello Flask App"
 
-@app.route('/user', methods=['GET'])
+@app.route('/user', methods=['GET', 'POST'])
 def users():
-    return UserController.index()
+    if request.method == 'GET':
+        return UserController.index()
+    else:
+        return UserController.save()
+
+# @app.route('/login', methods=['POST'])
+# def logins():
+#     return UserController.login()
